89bbfc9ac7f4ccd42234098e10c2df90f577caec
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-08 21:11:44 +0300

    add smooth to camera movement

--- Assets/_Scripts/PlayerTrecker.cs
+++ Assets/_Scripts/PlayerTrecker.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerTrecker : MonoBehaviour
 {
     [SerializeField] private PlayerTower _playerTower;
+    [SerializeField] private float _moveSpeed;
     [SerializeField] private Vector3 _offsetPosition;
     [SerializeField] private Vector3 _offsetRotation;
+
+    private Vector3 _targetOffsetPosition;
 
 
     private void OnEnable()
@@ -16,6 +19,7 @@ public class PlayerTrecker : MonoBehaviour
 
     private void OnDisable()
     {
+        
         _playerTower.CharacterAdded -= OnCharacterAdded;
     }
 
@@ -23,22 +27,28 @@ public class PlayerTrecker : MonoBehaviour
     private void Update()
     {
         UpdatePosition();
+        _offsetPosition = Vector3.MoveTowards(_offsetPosition, _targetOffsetPosition, _moveSpeed * Time.deltaTime);
     }
 
 
     private void UpdatePosition()
     {
+
+        //_targetPosition = _playerTower.transform.position + _offsetPosition;
+
         transform.position = _playerTower.transform.position;
         transform.localPosition += _offsetPosition;
 
         Vector3 lookAtPoint = _playerTower.transform.position + _offsetRotation;
+
+        //transform.localPosition = Vector3.MoveTowards(transform.localPosition, _targetPosition, _moveSpeed * Time.deltaTime);
 
         transform.LookAt(lookAtPoint);
     }
 
     private void OnCharacterAdded(int count)
     {
-        _offsetPosition += (Vector3.up + Vector3.back) * count;
+        _targetOffsetPosition = _offsetPosition + (Vector3.up + Vector3.back) * count;
         UpdatePosition();
     }
 }
